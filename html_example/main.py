0484ef54268a81b195af93af238482757e302569
Zach Ingbretsen <zingbretsen@ischool.berkeley.edu>
2017-04-22 02:05:15 +0000

    Better plotting/data selection

--- html_example/main.py
+++ html_example/main.py
@@ -21,36 +21,67 @@ def fetch_tables(search = None, limit = 10):
         return [table[0] for table in psql.cur.fetchall()]
 
 
+def get_col_names(table):
+    with PostgreSQL(table = table, database = 'pittsburgh') as psql:
+        query = "select column_name, data_type from information_schema.columns \
+            where table_name = '{}'".format(table)
+        psql.execute(query)
+        res = psql.cur.fetchall()
+    columns = [col[0] for col in res]
+    dtypes = [col[1] for col in res]
+    return columns, dtypes
 
-def get_df(psql, table):
-    query = "select column_name from information_schema.columns \
-    where table_name = '{}'".format(table)
-    psql.execute(query)
-    columns = [col[0] for col in psql.cur.fetchall()]
-    df = pd.DataFrame(psql.select())
-    df.columns = columns
+
+def get_df(table, cols = None, limit = None):
+    colnames, dtypes = get_col_names(table)
+    if cols is None:
+        columns = "*"
+    else:
+        columns = ','.join(cols)
+
+    if limit is not None and int(limit) > 0:
+        predicates = ' limit {}'.format(limit)
+    else:
+        predicates = ''
+        
+
+    with PostgreSQL(database = 'pittsburgh', table = table) as psql:
+        # query = "select {} from {}".format(columns, table, )
+        # psql.execute(query)
+        # columns = [col[0] for col in ]
+        df = pd.DataFrame(psql.select(table = table, cols = cols, predicates = predicates))
+        if cols is not None:
+            df.columns = list(cols)
+    # df = df[list(cols)]
+        # df.columns = columns
     return df
 
 
 def isNumeric(dtype):
-    print(dtype)
-    if str(dtype) in ('int64', 'float64'):
+    if str(dtype) in ('int64', 'float64', 'integer', 'double precision'):
         return True
     else:
         return False
 
+
 class Root:
     @cherrypy.expose
-    def index(self, table = 'weather', X = None, Y = None, search = None, limit = 10):
-        with PostgreSQL(table = table, database = 'pittsburgh') as psql:
-            df = get_df(psql, table)
+    def index(self, table = 'weather', X = None, Y = None, search = None, limit = 10, size = 1, table_limit = 10):
+        env = Environment(loader=FileSystemLoader('templates'))
+        tmpl = env.get_template('index.html')
+        tables = fetch_tables(search, table_limit)
+        colnames, dtypes = get_col_names(table)
 
-            tables = fetch_tables(search = search, limit = limit)
+        if table is not None:
+            df = get_df(cols = [x for x in (X, Y, ) if x is not None], table = table, limit = limit)
+
+        X = X if X in colnames else None
+        Y = Y if Y in colnames else None
 
         if X is None and Y is None:
             html = ''
             title = ''
-            
+
         elif Y is None or X is None:
             if X is None:
                 X = Y
@@ -67,25 +98,40 @@ class Root:
             plot = figure()
             plot.cross(df[X], df[Y])
             html = file_html(plot, CDN, "my plot")
-            
+
 
         env = Environment(loader=FileSystemLoader('templates'))
-        cols = tuple(df.columns)
-        dtypes = tuple(df.dtypes)
+        # cols = tuple(df.columns)
+        cols = colnames
+        # dtypes = tuple(df.dtypes)
         numeric = [col for col, dtype in zip(cols, dtypes) if isNumeric(dtype)]
         categorical = [col for col, dtype in zip(cols, dtypes) if not isNumeric(dtype)]
 
         tmpl = env.get_template('index.html')
-        print(tmpl)
+        del df
+
         return tmpl.render(salutation ='Pittsburgh: ',
-                           target     = title, 
-                           title      = "Pittsburgh",
-                           plot       = html,
-                           X    = numeric,
-                           Y    = numeric,
-                           groupby = categorical,
-                           tables = tables,
+                        target     = title, 
+                        title      = "Pittsburgh",
+                        plot       = html,
+                        X    = numeric,
+                        Y    = numeric,
+                        groupby = categorical,
+                        tables = tables,
         )
+
+        # except Exception as e:
+        #     print(e)
+        #     tmpl = env.get_template('index.html')
+        #     return tmpl.render(salutation ='Pittsburgh: ',
+        #                     target     = "Try Again", 
+        #                     title      = "Pittsburgh",
+        #                     plot       = "Could not complete your request",
+        #     )
+
+
+    def POST(self, table):
+        return "X"
 
 
     @cherrypy.expose
@@ -95,12 +141,17 @@ class Root:
         env = Environment(loader=FileSystemLoader('templates'))
 
         tmpl = env.get_template('map.html')
-        print(tmpl)
+
+        from bokeh.events import ButtonClick
+        from bokeh.models import Button, CustomJS
+
+        button = Button()
+
+        button.js_on_event(ButtonClick, CustomJS(code='console.log("JS:Click")'))
         return tmpl.render(salutation='Pittsburgh: ',
                            target='Median Age Map',
                            title = "Median Age",
                            webmap = webmap,
-                           sidebar = None,
         )
     
 
