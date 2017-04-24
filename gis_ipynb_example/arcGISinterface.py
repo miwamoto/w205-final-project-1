10764bf7615d401ef4eb2cef355b7a3cc72d3f55
miwamoto <mona_iwamoto@hotmail.com>
2017-04-24 19:17:26 +0000

    added SNAP data

--- gis_ipynb_example/arcGISinterface.py
+++ gis_ipynb_example/arcGISinterface.py
@@ -5,16 +5,22 @@ from IPython.display import display
 import arcgis
 from arcgis.gis import GIS
 
-ACCOUNT = 'miwamotoDEV'
+### This ingests the Pittsburgh data, creates map layers and publishes to ArcGIS online
+### These layers are useful for further geospatial analysis.
 
+
+ACCOUNT = 'miwamotoDEV'
+FORECASTFILE = 'forecasts.csv'
+FORECASTPATH = ''
+BASEDIR = '/data/pb_files'
 
 def logon_to_arcgis():
     
-    password = getpass.getpass("Enter password: ")
+    password = getpass.getpass('Enter password: ')
     try:
         global gis
-        gis = GIS("https://www.arcgis.com","miwamotoDEV", password)
-        print("Successfully logged in as: " + gis.properties.user.username)
+        gis = GIS('https://www.arcgis.com','miwamotoDEV', password)
+        print('Successfully logged in as: ' + gis.properties.user.username)
         return (True)
     except:
         print('Invalid password')
@@ -23,33 +29,27 @@ def logon_to_arcgis():
 
 def save_layer(properties, filepath, thumbnail):
     try:
-        return(gis.content.add(properties, data = filepath, thumbnail = thumbnail))
+        content = gis.content.add(properties, data = filepath, thumbnail = thumbnail)
+        print('Success ' + properties['title'] + ' imported into GIS')
+        return(content)
     except:
         print('file may already exist')
         search_str = 'title: ' + properties['title']
         search_results = gis.content.search(search_str, 'Feature Layer')
         try:
             return(search_results[0])
+            print('Success - ' + search_str + ' found')
         except:
-            print('layer not found in gis')
+            print(search_str + ' layer not found in GIS')
 
 
-def main():
+def testrun():
 
-    ### test logon
-    success = False
-    attempt = 0 
-    while not success and attempt <3:
-        success = logon_to_arcgis()
-        attempt += 1
-
-    ### testing getting a layer
-
-    csv_path = r"/data/pb_files/CSV/pittsburgh-police-arrest-data_0.csv"
+    csv_path = r'/data/pb_files/CSV/pittsburgh-police-arrest-data_0.csv'
     csv_properties = {'title': 'Pittsburgh Arrest Data',
                  'description':'Pittsburgh Arrest Data downloaded from data.gov',
                  'tags':'arcgis, python, Pittsburgh, arrests, crime'}
-    thumbnail_path = r"handcuffs.png"
+    thumbnail_path = r'handcuffs.png'
     
     crime_csv_item = save_layer(csv_properties, csv_path, thumbnail_path)
     
@@ -67,6 +67,76 @@ def main():
     ### List feature fields
     for f in crime_feature_layer.properties.fields:
         print(f['name'])
+ 
+
+def main():
+    
+    test = False
+
+    ### Log on to ArcGIS account  
+    success = False
+    attempt = 0 
+    while not success and attempt <3:
+        success = logon_to_arcgis()
+        attempt += 1
+
+    ### testing getting a layer
+    if (test): testrun()
+        
+    
+    ### Import Neighborhood GeoSpacial Info
+    csv_path = BASEDIR + '/ZIP/pittsburgh_neighborhoods_64095_5.zip'
+    csv_properties = {'title': 'Pittsburgh Neighborhoods',
+                 'description':'Pittsburgh Neighborhood Shapefile downloaded from data.gov',
+                 'tags':'arcgis, python, Pittsburgh, neighborhood'}
+    thumbnail_path = r'neighborhood-icon.png'
+
+    neighborhood_item = save_layer(csv_properties, csv_path, thumbnail_path)
+    try:
+        neighborhood_layer = neighborhood_item.publish()
+        print('Success - published')
+    except:
+        print('Shapefile already published')
+ 
+
+    ### Import Neighborhood GeoSpacial SNAP Data     
+    csv_path = BASEDIR + '/ZIP/neighborhoods_with_snap_data_3d3a9_5.zip' 
+    csv_properties = {'title': 'Pittsburgh Neighborhoods with SNAP Data',
+                     'description':'Pittsburgh Neighborhood SNAP data downloaded from data.gov',
+                     'tags':'arcgis, python, Pittsburgh, neighborhood'}
+    thumbnail_path = 'neighborhood-icon.png'
+
+    neighborhood_item = save_layer(csv_properties, csv_path, thumbnail_path)
+    try:
+        neighborhood_layer = neighborhood_item.publish()
+        print('Success - published')
+    except:
+        print('CSV already published')
+   
+
+    ### Import latest Incident Probability Data
+    csv_path = FORECASTPATH+ FORECASTFILE
+    csv_properties = {'title': 'Pittsburgh Incident Forecast',
+                 'description':'Pittsburgh Incident Forecast',
+                 'tags':'arcgis, python, Pittsburgh, arrests, crime'}
+    thumbnail_path = r'handcuffs.png'
+
+    crime_csv_item = save_layer(csv_properties, csv_path, thumbnail_path)
+    try:
+        forecast_layer = crime_csv_item.publish()
+        print('Success - published')
+    except:
+        print('Layer already published')
+        
+    ### layer manipulation
+    #map1 = gis.map('Pittsburgh, PA', zoomlevel = 13)
+    #search_results = gis.content.search('title: Pittsburgh Incident Forecast',
+    #                                'Feature Layer')
+    #search_results
+    #Incident_item = search_results[0]
+    #map1.add_layer(Incident_item)
+    
+        
 
 
 if __name__ == '__main__':
